80070d89a132d9918f85b0dfe1144c9047328294
Phil Zona <pzona@launchdarkly.com>
2022-12-22 17:25:46 -0500

    Missing the ready event?

--- client/index.js
+++ client/index.js
@@ -4,12 +4,12 @@ const LD = require('./launchdarkly');
 exports.startSimulation = async (numberOfClients) => {
   console.log('Starting simulation');
 
-  LD.on('ready', () => {
-    console.log('LD client ready for use');
+  // LD.on('ready', () => {
+  //   console.log('LD client ready for use');
 
     const clientmanager = new ClientManager(numberOfClients);
     clientmanager.start();
-  })
+  // })
   
   // Todo: implement this
 }
